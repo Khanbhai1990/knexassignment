01dd342e605dc73a8d979b16eda2012451de9857
bdoring <doringbarbara@gmail.com>
2017-10-14 13:34:50 -0700

    all pages have been minimally styled

---
+++ static/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  console.log('linked');
+
+  let newUserForm = $('.myForm')[0];
+  $(newUserForm).hide();
+
+  let addUserButton = $('#add-user-button');
+  $(addUserButton).click(function(event) {
+    event.preventDefault();
+    $(newUserForm).slideDown(1000);
+  });
+
+  let cancelButton = $('#cancel-button');
+  $(cancelButton).click(function(event) {
+    event.preventDefault();
+    $(newUserForm).slideUp(1000);
+
+  });
+
+
+});
